97df942b7c7efc2d68e91a117b31ce5ff03f6c12
LucasRotermel <lucas.rotermel.franco@gmail.com>
2024-10-31 22:15:14 -0300

    feat: Adding execute of delete command

--- src/main/java/LucasRotermel/TaskTracker/Commands/Delete.java
+++ src/main/java/LucasRotermel/TaskTracker/Commands/Delete.java
@@ -1,5 +1,8 @@
 package LucasRotermel.TaskTracker.Commands;
 
+import LucasRotermel.TaskTracker.Exceptions.ArgumentQuantityException;
+import LucasRotermel.TaskTracker.Exceptions.ArgumentTypeException;
+import LucasRotermel.TaskTracker.Exceptions.IdentifierInexistentException;
 import LucasRotermel.TaskTracker.Exceptions.IncorrectExecuteException;
 import LucasRotermel.TaskTracker.Manager.Task;
 
@@ -7,9 +10,35 @@ import java.util.List;
 
 public class Delete extends Command{
     @Override
-    public List<Task> execute(List<Task> file, String[] args) {
-        System.out.println("Hello Delete");
-        return file;
+    public List<Task> execute(List<Task> tasks, String[] args) {
+        if (args.length > 2)
+            throw new ArgumentQuantityException(args.length - 1, "delete", "1");
+
+        int idx;
+
+        try {
+            idx = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new ArgumentTypeException();
+        }
+
+        boolean idxExists = false;
+
+        for (Task task : tasks) {
+            if (task.getId() == idx) {
+                idxExists = true;
+                break;
+            }
+        }
+
+        if (!idxExists)
+            throw new IdentifierInexistentException();
+
+        idx = idx - 1;
+
+        tasks.remove(idx);
+
+        return tasks;
     }
 
     @Override
